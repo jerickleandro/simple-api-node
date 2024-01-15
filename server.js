531d54d5990c57d2ed6d8b4038fa6a1e732c9971
Erick Leandro <j.erickleandro@gmail.com>
2024-01-15 15:41:27 +0000

    chore: adicionando rotas

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 import http from "http";
 const PORT = 3000;
+
+const rotas = {
+    "/": "Simple API Node!",
+    "/livros": "Entrando na rota livros",
+    "/autores": "Entrando na rota autores"
+}
 const server = http.createServer((req, res) => {
     res.writeHead(200, {
         "Content.Type": "text/plain"
     });
-    res.end("Simple API Node");
+    res.end(rotas[req.url]);
 });
 
 server.listen(PORT, () => {
