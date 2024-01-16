6463f10c2f45a17ee778a897632e529af994bf4a
Erick Leandro <j.erickleandro@gmail.com>
2024-01-16 12:03:36 +0000

    feat: adicionado rota post livros

--- src/app.js
+++ src/app.js
@@ -1,9 +1,30 @@
 import express from "express";
 
 const app = express();
+app.use(express.json())
+
+const livros = [
+    {
+        id: 1,
+        titulo: "O Senhor dos Aneis"
+    },
+    {
+        id: 2,
+        titulo: "O Hobbit"
+    }
+]
 
 app.get("/", (req, res) => {
     res.status(200).send("API Node Simples para Exemplos");
 });
+
+app.get("/livros", (req, res) => {
+    res.status(200).json(livros)
+})
+
+app.post("/livros", (req, res) => {
+    livros.push(req.body);
+    res.status(201).json({ mensagem: "Livro cadastrado com Sucesso!" });
+})
 
 export default app;
