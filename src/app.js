3e505a493419e77545b8503536f96731f680213e
Erick Leandro <j.erickleandro@gmail.com>
2024-01-16 15:58:12 +0000

    feat: adicionar rotas de put e delete

--- src/app.js
+++ src/app.js
@@ -27,4 +27,21 @@ app.post("/livros", (req, res) => {
     res.status(201).json({ mensagem: "Livro cadastrado com Sucesso!" });
 })
 
+app.put("/livros/:id", (req, res) => {
+    const { id } = req.params;
+    const updateBook = req.body;
+    const indice = livros.findIndex(obj => Number(obj.id) === Number(id));
+    livros[indice] = updateBook;
+    res.status(200).json({ mensagem: "Livro editado com Sucesso!" });
+})
+
+app.delete("/livros/:id", (req, res) => {
+    const { id } = req.params;
+    const indice = livros.findIndex(obj = Number(obj.id) === Number(id));
+    if (indice !== -1) {
+        array.splice(indice, 1);
+        res.status(200).json({ mensagem: "Livro deletado com Sucesso!" });
+    }
+    res.status(404).json({ mensagem: "Não foi encontrado livro com esse id" });
+});
 export default app;
