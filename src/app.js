9d1c71d0998295de1b3b41827437ae33ecc02aeb
Erick Leandro <j.erickleandro@gmail.com>
2024-01-16 17:47:07 +0000

    feat: add nova rota busca livro by id

--- src/app.js
+++ src/app.js
@@ -13,6 +13,9 @@ const livros = [
         titulo: "O Hobbit"
     }
 ]
+const buscaIndiceLivroPorId = (id) => {
+    return livros.findIndex(obj = Number(obj.id) === Number(id))
+}
 
 app.get("/", (req, res) => {
     res.status(200).send("API Node Simples para Exemplos");
@@ -22,6 +25,15 @@ app.get("/livros", (req, res) => {
     res.status(200).json(livros)
 })
 
+app.get("/livro/:id", (req, res) => {
+    const { id } = req.params;
+    const indice = buscaIndiceLivroPorId(id)
+    if (indice !== -1) {
+        res.status(200).json({ livro: livros[indice] });
+    }
+    res.status(404);
+});
+
 app.post("/livros", (req, res) => {
     livros.push(req.body);
     res.status(201).json({ mensagem: "Livro cadastrado com Sucesso!" });
@@ -30,14 +42,14 @@ app.post("/livros", (req, res) => {
 app.put("/livros/:id", (req, res) => {
     const { id } = req.params;
     const updateBook = req.body;
-    const indice = livros.findIndex(obj => Number(obj.id) === Number(id));
+    const indice = buscaIndiceLivroPorId(id);
     livros[indice] = updateBook;
     res.status(200).json({ mensagem: "Livro editado com Sucesso!" });
 })
 
 app.delete("/livros/:id", (req, res) => {
     const { id } = req.params;
-    const indice = livros.findIndex(obj = Number(obj.id) === Number(id));
+    const indice = buscaIndiceLivroPorId(id);
     if (indice !== -1) {
         array.splice(indice, 1);
         res.status(200).json({ mensagem: "Livro deletado com Sucesso!" });
